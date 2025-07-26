0bdeaa2a44f44e1dea90bcdfeecd72fe0053afbe
jabezinu <jabezzinu31@gmail.com>
2025-07-26 13:18:51 +0300

    Implement API Logic

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -7,59 +7,30 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        return Task::orderBy('created_at','desc')->get();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate(['title' => 'required|string']);
+        return Task::create($data);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Task $task)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Task $task)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Task $task)
     {
-        //
+        $data = $request->validate([
+            'title' => 'sometimes|required|string',
+            'completed' => 'sometimes|required|boolean',
+        ]);
+        $task->update($data);
+        return $task;
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+        return response(null, 204);
     }
 }
